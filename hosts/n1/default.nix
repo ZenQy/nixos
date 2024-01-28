64748c61b04920dff8d5892c6856951a7a9c45f2
ZenQy <zenqy.qin@gmail.com>
2024-01-28 11:59:43 +0800

    add .

--- hosts/n1/default.nix
+++ hosts/n1/default.nix
@@ -12,7 +12,7 @@
   systemd.network.networks.default = {
     name = "eth0";
     address = [ "10.0.0.9/24" ];
-    gateway = [ "10.0.0.1" ];
+    gateway = [ "10.0.0.11" ];
     dns = [ "114.114.114.114" ];
     DHCP = "no";
   };
--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -129,7 +129,7 @@
 
     cargo
     rustc
-    rustfmt
+    rust-analyzer
 
     gcc
     leetgo
