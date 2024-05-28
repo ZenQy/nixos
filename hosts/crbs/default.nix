4c2f8176f14c034be145a8d2ded6637c5de4df82
ZenQy <zenqy.qin@gmail.com>
2024-05-28 18:27:59 +0800

    fix crbs ipv6

--- hosts/crbs/default.nix
+++ hosts/crbs/default.nix
@@ -8,10 +8,16 @@
   systemd.network.networks.default = {
     name = "enp3s0";
     address = secrets.crunchbits.address;
-    gateway = secrets.crunchbits.gateway;
     dns = [ "8.8.8.8" "2001:4860:4860::8888" ];
-    DHCP = "no";
-    extraConfig = "IPv6AcceptRA=no";
+    routes = [
+      { routeConfig.Gateway = secrets.crunchbits.gateway.ipv4; }
+      {
+        routeConfig = {
+          Gateway = secrets.crunchbits.gateway.ipv6;
+          GatewayOnLink = true;
+        };
+      }
+    ];
   };
 
 }
