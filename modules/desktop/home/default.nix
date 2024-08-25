a63d377ec5a4a155ef74c9b330ba64c14e09435a
ZenQy <zenqy.qin@gmail.com>
2024-08-25 22:05:37 +0800

    update

--- modules/desktop/home/default.nix
+++ modules/desktop/home/default.nix
@@ -7,31 +7,28 @@ let
     vscode = {
       file = toFile "settings.json" (toJSON (import ./vscode.nix));
       path = ".config/VSCodium/User/settings.json";
-      link = false;
     };
     niri = {
       file = toString ./niri-config.kdl;
       path = ".config/niri/config.kdl";
-      link = true;
     };
   };
-  ln = map
+  cmd = map
     (name:
       let
-        symlink = "/home/nixos/" + conf.${name}.path;
+        fullpath = "/home/nixos/" + conf.${name}.path;
         source = conf.${name}.file;
-        target = substring 0 ((stringLength symlink) - (stringLength (baseNameOf symlink))) symlink;
-        cmd = if conf.${name}.link then "ln -sf ${source} ${symlink}" else "cat ${source} > ${symlink}";
+        target = substring 0 ((stringLength fullpath) - (stringLength (baseNameOf fullpath))) fullpath;
       in
       ''
         mkdir -p ${target}
-        ${cmd}
+        cat ${source} > ${fullpath}
       '')
     (attrNames conf);
 
   script = ''
     #!/usr/bin/env bash
-  '' + concatStringsSep "\n" ln;
+  '' + concatStringsSep "\n" cmd;
 
 in
 {
