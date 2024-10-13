f2a207dbe5695d3a965aad6b6797d8c58c88dadd
ZenQy <zenqy.qin@gmail.com>
2024-10-13 14:49:51 +0800

    add vite

---
+++ pkgs/others/vitejs/default.nix
@@ -0,0 +1,49 @@
+{
+  source,
+  lib,
+  stdenv,
+  pnpm,
+  nodejs,
+}:
+
+stdenv.mkDerivation {
+  inherit (source) pname version src;
+
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (source) pname version src;
+    hash = "sha256-BzB7o4JmQr8w0qiDcZ3T0HLC5H0LyVEk+KhtHsIroLk=";
+  };
+
+  nativeBuildInputs = [
+    pnpm.configHook
+  ];
+
+  buildInputs = [
+    nodejs
+  ];
+
+  buildPhase = ''
+    pnpm run build
+  '';
+
+  installPhase = ''
+    mkdir -p $out/{bin,lib}
+    cp -r ./* $out/lib
+
+    ln -sf $out/lib/packages/vite/bin/vite.js $out/bin/vite
+    ln -sf $out/lib/packages/create-vite/index.js $out/bin/create-vite
+  '';
+
+  meta = with lib; {
+    description = "Next generation frontend tooling.";
+    homepage = "https://vite.dev/";
+    license = licenses.mit;
+    maintainers = [
+      {
+        name = "ZenQy";
+        email = "zenqy.qin@gmail.com";
+      }
+    ];
+    platforms = platforms.all;
+  };
+}
